d1054ab9bb2066321f423ad71c1e03cf35b39f97
Beni Young <55400759+beni-young@users.noreply.github.com>
2022-12-02 06:57:10 -0600

    testing map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, StyleSheet} from 'react-native';
-import MapView, {Polyline} from 'react-native-maps';
+import MapView, {Polyline, Marker} from 'react-native-maps';
 
 const Map = () => {
 
-    let points = [];
-    for(let i = 0; i < 20; i++){
-        points.push({
-            latitude: 30.0273,
-            longitude: -90.0680
-        })
-    }
+    const[mapRegion, setMapRegion] = useState ({
+        latitude: 30.01947000085054, 
+        longitude: -90.06525841636753       
+    });
+
+    const[testRegion, setTestRegion] = useState ({
+        latitude: 30.020515134607734, 
+        longitude: -90.06645451835102       
+    });
+
+    const[schoolRegion, setSchoolRegion] = useState ({
+        latitude: 30.023432002000366, 
+        longitude: -90.06559621153748
+    });    
 
     return <MapView style={styles.map} 
             initialRegion= {{
@@ -19,8 +26,13 @@ const Map = () => {
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01,
             }}   
-    >
-        <Polyline coordinates={points} />
+    >       
+        <Marker draggable coordinate={{latitude: 30.01947000085054, longitude: -90.06525841636753}}
+            title={"Hello!!!"}            
+         />
+         <Marker coordinate={testRegion} title={"Test!!!"} />
+         <Marker coordinate={schoolRegion} title={"School!!!"} />
+         
     </MapView>
 };
 
